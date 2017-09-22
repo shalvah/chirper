81e7fd28eeca24cb623857dc06898ee4e47a5a27
Shalvah Adebayo <shalvah.adebayo@konga.com>
2017-09-22 19:31:47 +0100

    Setup Pusher and broadcasting config

--- config/app.php
+++ config/app.php
@@ -173,7 +173,7 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
@@ -225,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Pusher' => Pusher\Pusher::class
 
     ],
 
